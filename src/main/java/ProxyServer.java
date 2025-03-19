097ef102b62f7f2c3fd1d2b38e549dd804c14fda
cuceta <cuceta@oswego.edu>
2025-03-18 20:59:01 -0400

    Create ProxyServer.java

---
+++ src/main/java/ProxyServer.java
@@ -0,0 +1,15 @@
+import java.net.*;
+import java.io.*;
+
+public class ProxyServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080); // Proxy listens on port 8080
+        System.out.println("Proxy Server started on port 8080");
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getInetAddress());
+            new Thread(new ClientHandler(clientSocket)).start();
+        }
+    }
+}
