d9f8a57db55629019f743220d1881cfae14ae72f
cuceta <cuceta@oswego.edu>
2025-03-24 09:54:23 -0400

    URL is hard coded

--- src/main/java/tryTwo/Client.java
+++ src/main/java/tryTwo/Client.java
@@ -1,25 +1,25 @@
 package tryTwo;
+
 import java.io.*;
 import java.net.Socket;
 
 public class Client {
     private static final String PROXY_HOST = "localhost";
     private static final int PROXY_PORT = 8080;
+    // Hard-coded URL
+    private static final String URL = "https://www.reddit.com/r/aww/comments/r0rk0e/arent_chow_chow_puppies_so_cute/#lightbox";
+    
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage: java Client <URL>");
-            return;
-        }
-
-        String url = args[0];
-
         try (Socket socket = new Socket(PROXY_HOST, PROXY_PORT);
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              InputStream in = socket.getInputStream();
              FileOutputStream fileOut = new FileOutputStream("downloaded_file")) {
 
-            out.println(url);
+            // Send the URL to the proxy
+            out.println(URL);
+
+            // Read the response and save to a file
             byte[] buffer = new byte[4096];
             int bytesRead;
             while ((bytesRead = in.read(buffer)) != -1) {
