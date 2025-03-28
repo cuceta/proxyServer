c60a1390ed428d6345af86269d569e89afb64c06
cuceta <cuceta@oswego.edu>
2025-03-28 13:15:26 -0400

    Update Client.java

--- src/main/java/tryTwo/Client.java
+++ src/main/java/tryTwo/Client.java
@@ -12,9 +12,11 @@ public class Client {
 //    private static final String URL = "https://blogs.biomedcentral.com/bmcseriesblog/wp-content/uploads/sites/9/2017/03/ChowChow2Szczecin.jpg";
 
     // FLOWERS IMAGE
-    private static final String URL = "https://images.contentstack.io/v3/assets/bltcedd8dbd5891265b/blt682575f35cf2888d/6668cee126c2a688bd1aa8b1/Birthday-Flowers-Colors.jpg?q=70&width=1200&auto=webp.jpg";
+//    private static final String URL = "https://images.contentstack.io/v3/assets/bltcedd8dbd5891265b/blt682575f35cf2888d/6668cee126c2a688bd1aa8b1/Birthday-Flowers-Colors.jpg?q=70&width=1200&auto=webp.jpg";
 
     //BEACH
+    private static final String URL = "https://images.contentstack.io/v3/assets/bltcedd8dbd5891265b/blt682575f35cf2888d/6668cee126c2a688bd1aa8b1/Birthday-Flowers-Colors.jpg?q=70&width=1200&auto=webp.jpg";
+
     public static void main(String[] args) {
         try (Socket socket = new Socket(PROXY_HOST, PROXY_PORT);
              DataOutputStream out = new DataOutputStream(socket.getOutputStream());
