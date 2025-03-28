23341cd13af47340c9f7bc0a85db9699e695aa96
cuceta <cuceta@oswego.edu>
2025-03-28 13:03:04 -0400

    Update Client.java

--- src/main/java/tryTwo/Client.java
+++ src/main/java/tryTwo/Client.java
@@ -8,7 +8,7 @@ public class Client {
     private static final String PROXY_HOST = "localhost";
     private static final int PROXY_PORT = 8080;
     // Hard-coded URL to request
-    private static final String URL = "https://images.contentstack.io/v3/assets/bltcedd8dbd5891265b/blt682575f35cf2888d/6668cee126c2a688bd1aa8b1/Birthday-Flowers-Colors.jpg?q=70&width=1200&auto=webp.jpg";
+    private static final String URL = "https://blogs.biomedcentral.com/bmcseriesblog/wp-content/uploads/sites/9/2017/03/ChowChow2Szczecin.jpg";
 
     public static void main(String[] args) {
         try (Socket socket = new Socket(PROXY_HOST, PROXY_PORT);
