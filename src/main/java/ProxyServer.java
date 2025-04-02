1931bd0c321dc715184c67b0c6b4cea00f3fc133
cuceta <cuceta@oswego.edu>
2025-04-02 12:43:34 -0400

    rho to moxie data collected

--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ProxyServer {
-    private static final int PORT = 8080;
+    private static final int PORT = 26896;
     private static final int THREAD_POOL_SIZE = 10;
 
     public static void main(String[] args) {
