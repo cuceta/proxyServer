1b3616d969982c6c1dc4624eb7507adda5a0eb5f
cuceta <cuceta@oswego.edu>
2025-04-05 20:04:11 -0400

    Update Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,9 +3,9 @@ import java.net.Socket;
 import java.util.Random;
 
 public class Client {
-    private static final String PROXY_HOST = "localhost";
+//    private static final String PROXY_HOST = "localhost";
 
-//    private static final String PROXY_HOST = "rho.cs.oswego.edu";
+    private static final String PROXY_HOST = "rho.cs.oswego.edu";
 //    private static final String PROXY_HOST = "moxie.cs.oswego.edu";
 
     private static final int PROXY_PORT = 26896;
