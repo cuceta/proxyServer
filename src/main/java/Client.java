35b2006d3e611bb1ad766331364a6099ebd47979
cuceta <cuceta@oswego.edu>
2025-04-07 10:29:56 -0400

    csv files throughput results

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,35 +4,21 @@ import java.util.Random;
 
 public class Client {
     private static final String PROXY_HOST = "localhost";
-
-//    private static final String PROXY_HOST = "rho.cs.oswego.edu";
-//    private static final String PROXY_HOST = "moxie.cs.oswego.edu";
-
     private static final int PROXY_PORT = 26896;
 
-    // CHOW CHOW IMAGE
-//    private static final String URL = "https://blogs.biomedcentral.com/bmcseriesblog/wp-content/uploads/sites/9/2017/03/ChowChow2Szczecin.jpg";
-
-    // FLOWERS IMAGE
-//    private static final String URL = "https://images.contentstack.io/v3/assets/bltcedd8dbd5891265b/blt682575f35cf2888d/6668cee126c2a688bd1aa8b1/Birthday-Flowers-Colors.jpg?q=70&width=1200&auto=webp.jpg";
-
-    //BEACH
-//    private static final String URL = "https://www.celebritycruises.com/blog/content/uploads/2021/07/best-beaches-in-dominican-republic-playa-la-ensenada-1600x890.jpg";
-
-    //ICE CREAM
-    private static  String URL;
-    private static  String drop;
+    // ICE CREAM image URL is now provided via command line arguments.
+    private static String URL;
+    private static String drop;
     private static String windowSize;
-    private static  String host_to_server;
-    public static void main(String[] args) {
+    private static String host_to_server;
 
+    public static void main(String[] args) {
         URL = args[0];
         windowSize = args[1];
         drop = args[2];
         host_to_server = args[3];
 
         try (Socket socket = new Socket(PROXY_HOST, PROXY_PORT);
-
              DataOutputStream out = new DataOutputStream(socket.getOutputStream());
              DataInputStream in = new DataInputStream(socket.getInputStream())) {
 
@@ -48,13 +34,12 @@ public class Client {
             int encryptionKey = clientRandom ^ serverRandom;
             System.out.println("Encryption Key Established: " + encryptionKey);
 
-            // --- Send the URL (hard-coded) ---
+            // --- Send the URL ---
             out.writeUTF(URL);
             out.flush();
 
-
             // Determine file name based on URL.
-            String fileName = "drop_"+drop+"_"+windowSize+"_"+sanitizeFileName(URL);
+            String fileName = "drop_" + drop + "_" + windowSize + "_" + sanitizeFileName(URL);
             System.out.println("Saving downloaded file as: " + fileName);
 
             // --- Measure Throughput ---
@@ -63,10 +48,9 @@ public class Client {
             long endTime = System.nanoTime();
 
             // Calculate file size and throughput in Mbps.
-            File downloadedFile = new File(host_to_server+ File.separator +"temp", fileName);
+            File downloadedFile = new File(host_to_server + File.separator + "temp", fileName);
             long fileSizeBytes = downloadedFile.length();
             double elapsedSeconds = (endTime - startTime) / 1e9;
-            // Convert file size to bits and then calculate Mbps.
             double throughputBps = (fileSizeBytes * 8) / elapsedSeconds;
             double throughputMbps = throughputBps / 1e6;
 
@@ -74,11 +58,11 @@ public class Client {
             System.out.println("Elapsed Time (seconds): " + elapsedSeconds);
             System.out.println("Throughput (Mbps): " + throughputMbps);
 
-            // Generate an HTML report with throughput information.
+            // Generate an HTML report with throughput information and save CSV.
             generateReport(fileSizeBytes, elapsedSeconds, throughputMbps, downloadedFile);
 
             // --- File Integrity Validation using cmp ---
-            String downloadedFilePath = host_to_server + File.separator+"temp" + File.separator + fileName;
+            String downloadedFilePath = host_to_server + File.separator + "temp" + File.separator + fileName;
             String referenceFilePath = "reference" + File.separator + sanitizeFileName(URL);
             validateFileIntegrity(downloadedFilePath, referenceFilePath);
 
@@ -121,7 +105,6 @@ public class Client {
         }
     }
 
-
     private static String sanitizeFileName(String url) {
         // Remove the protocol part and replace non-alphanumeric characters with underscores.
         String sanitized = url.replaceAll("https?://", "");
@@ -131,7 +114,6 @@ public class Client {
 
     private static void validateFileIntegrity(String downloadedFilePath, String referenceFilePath)
             throws IOException, InterruptedException {
-        // Build the cmp command.
         String command = "cmp " + downloadedFilePath + " " + referenceFilePath;
         System.out.println("Validating file integrity with command: " + command);
         Process process = Runtime.getRuntime().exec(command);
@@ -157,31 +139,39 @@ public class Client {
         if (!resultDir.exists()) {
             resultDir.mkdirs();
         }
-        String htmlFileName = "drop_" + drop + "_" + windowSize + "_throughput.html";
+        // Create a base name for both the HTML and CSV files.
+        String baseName = "drop_" + drop + "_" + windowSize + "_throughput";
+        String htmlFileName = baseName + ".html";
         String htmlContent =
-                "<div style='padding: 25px 50px;'> \n " +
-                        "<h1>Throughput Report for simulation with " + drop + " drop simulation and a " + windowSize + " window size.</h1> \n" +
-                        "<div style='display: flex; flex-wrap: wrap;'> \n" +
-                        "<div style='flex: 0 0 40%;'> \n" +
-                        "<img src='" + downloadedFile + "' style='width: 100%; height: auto;'/> \n" +
-                        "</div> \n" +
-                        "<div style='flex: 1; padding-left: 20px; align-content:end; '> \n" +
-                        "<p>File Size: " + fileSize + " bytes</p> \n" +
-                        "<p>Elapsed Time: " + String.format("%.3f", elapsedSeconds) + " seconds</p> \n" +
-                        "<p>Throughput: " + throughputMbps + " Mbps</p> \n" +
-                        "</div>\n" +
+                "<div style='padding: 25px 50px;'> \n" +
+                        "  <h1>Throughput Report for simulation with " + drop + " drop simulation and a " + windowSize + " window size.</h1> \n" +
+                        "  <div style='display: flex; flex-wrap: wrap;'> \n" +
+                        "    <div style='flex: 0 0 40%;'> \n" +
+                        "      <img src='" + downloadedFile + "' style='width: 100%; height: auto;'/> \n" +
+                        "    </div> \n" +
+                        "    <div style='flex: 1; padding-left: 20px; align-content:end;'> \n" +
+                        "      <p>File Size: " + fileSize + " bytes</p> \n" +
+                        "      <p>Elapsed Time: " + String.format("%.3f", elapsedSeconds) + " seconds</p> \n" +
+                        "      <p>Throughput: " + throughputMbps + " Mbps</p> \n" +
+                        "    </div>\n" +
+                        "  </div> \n" +
+                        "</div> \n";
 
-                        "</div> \n"
-                ;
-
-
-        File outputFile = new File(resultDir, htmlFileName);
-
-        try (FileWriter writer = new FileWriter(outputFile)) {
+        // Save the HTML report.
+        File htmlFile = new File(resultDir, htmlFileName);
+        try (FileWriter writer = new FileWriter(htmlFile)) {
             writer.write(htmlContent);
         }
-
         System.out.println("Throughput report generated: " + htmlFileName);
-    }
 
+        // Save the throughput data in a CSV file.
+        String csvFileName = baseName + ".csv";
+        File csvFile = new File(host_to_server, csvFileName);
+        try (FileWriter csvWriter = new FileWriter(csvFile)) {
+            // Write header and data row.
+            csvWriter.write("FileSize,ElapsedTime,Throughput\n");
+            csvWriter.write(fileSize + "," + elapsedSeconds + "," + throughputMbps + "\n");
+        }
+        System.out.println("Throughput data saved to CSV: " + csvFileName);
+    }
 }
